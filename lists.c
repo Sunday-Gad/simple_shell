190c8983d98f3136f67d53b98a7ead4e1e8cac76
Sunday Gad <57796111+Sunday-Gad@users.noreply.github.com>
2022-09-08 01:19:55 +0100

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,11 +1,14 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
+ * add_node - adds a node to the start of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
  *
  * Return: size of list
  */
+
 list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
